66df289e2115813dc14114376eedb9ee1fbcfd15
Victor Hugo <victorblog410@gmail.com>
2023-06-28 16:58:39 -0300

    feature adding repository mongodb

---
+++ repository/handler_db.py
@@ -0,0 +1,30 @@
+import pymongo
+from pymongo.results import InsertOneResult
+
+
+class MongoPipeline:
+    collection_name = "scrapy_items"
+
+    def __init__(self):
+        self.client = pymongo.MongoClient("mongodb://root:password@mongo")
+        self.mongo_db = self.client["food"]
+
+    def process_item(self, item) -> InsertOneResult:
+        return self.mongo_db[self.collection_name].insert_one(item)
+
+    def get_foods(self, limit):
+        try:
+            food = list(self.mongo_db[self.collection_name].find({}, {'_id': 0}).limit(limit))
+            return food
+        except Exception as e:
+            print(f"Error occurred while fetching brands from the database: {str(e)}")
+            return None
+
+    def get_food(self, barcode: str):
+        try:
+            code_data = self.mongo_db[self.collection_name].find_one({'barcode': barcode}, {'_id': 0})
+            return code_data
+        except Exception as e:
+            print(f"Error occurred while fetching data from the database: {str(e)}")
+            return None
+
